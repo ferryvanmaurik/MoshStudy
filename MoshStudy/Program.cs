eeb9a78d4e4d602288611bd4faf9654953b7fd79
ferryvanmaurik <ferryvanmaurik@msn.com>
2017-10-04 08:57:45 +0200

    Test update name to Ferry

--- MoshStudy/Program.cs
+++ MoshStudy/Program.cs
@@ -6,7 +6,7 @@ namespace MoshStudy
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ferry!");
         }
     }
 }
