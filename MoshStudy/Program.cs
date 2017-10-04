0feabd282655c41fd430790ca31acf46ba8db782
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-10-04 08:36:35 +0200

    First commit

---
+++ MoshStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoshStudy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
